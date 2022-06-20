a220eaa762832348213286e8e07896417d3d009e
becker63 <johnsontaylor6320@gmail.com>
2022-06-19 20:00:19 -0400

    1.4.3

--- src/annotation.js
+++ src/annotation.js
@@ -101,10 +101,10 @@ export default {
       args.changed = true;
     }
 
-    if(args.event.type != 'click'){
+    //if(args.event.type != 'click'){
 
       console.log('\n\noptions:',typeof(options),'\n', options, '\n\nargs:',typeof(args),'\n', args, '\n\nstate:',typeof(state),'\n', state, '\n\nchart:',typeof(chart),'\n', chart);
-    }
+   // }
   },
 
   destroy(chart) {
